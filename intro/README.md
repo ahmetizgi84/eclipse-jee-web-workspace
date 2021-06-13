2b7724e526df7a85fc6c4d590811af4fddc80f71
ahmetizgi <dev.ahmetizgi@gmail.com>
2021-06-13 14:40:37 +0300

    intro Readme.md

---
+++ intro/README.md
@@ -0,0 +1,31 @@
+## Java OOP Giriş
+
+- Nesne, insan zihninin kendisine yöneldiği özellik ve davranışlara sahip, fiziksel olan ya da olmayan herhangi bir olgudur.
+
+- Yazılımda, her tip nesne için, verisi (variables) ile davranışını (methods) bir paket haline getirip sarmalayan (encapsulation) 
+ve adına genelde sınıf (class) denen kalıplar oluşturulur. 
+
+    Sınıf = veri + davranış
+
+- Nesneler, sınıflardan türetilmiş çalışma zamanı yapılarıdır. 
+
+### Java Nesne Merkezlidir
+
+- Javada basit veri tipleri hariç herşey, bütün tipler bir nesnedir. 
+
+- Çok yönlü kapsülleme (encapsulation) ve bilgi saklama (information hidiing) mekanizmaları vardır.
+
+### Java Yorumlanır
+
+- Java hem derlenen (compiled) hem de yorumlanan (interpreted) bir dildir.
+
+- Platformdan bağımsız olabilmesi için Java kaynak kodları doğrudan makina koduna derlenmez.
+
+- Java kaynak kodları (.java dosyaları) java bytecode'una (.class dosyaları) derlenir.
+
+- Bytecode JVM'in komutlarıdır ve çalışma zamanında (runtime) JVM tarafından yorumlanır.
+
+- JVM ise (Java Virtual Machine) donanım ve işletim sistemi üzerinde çalışan sanal bir platformdur. 
+
+
+myProgram.java -> compiler (javac) -> myProgram.class (bytecode) -> JVM (JDK içinde geliyor) -> 0101011100
--- intro/src/intro/Main.java
+++ intro/src/intro/Main.java
@@ -5,33 +5,33 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String subeButonu = "İnternet Şubesi";
+		String subeButonu = "Ä°nternet Åubesi";
 		System.out.println(subeButonu);
 		
 		String[] krediler = {
-				"Hızlı Kredi",
+				"HÄ±zlÄ± Kredi",
 				"Mutlu Emekli Kredisi",
 				"Konut Kredisi",
-				"Çiftçi Kredisi",
+				"Ã‡iftÃ§i Kredisi",
 				"Msb Kredisi",
 				"Meb Kredisi",
-				"İhtiyaç Kredisi",
+				"Ä°htiyaÃ§ Kredisi",
 		};
 		
-		// foreach döngüsü
+		// foreach dÃ¶ngÃ¼sÃ¼
 		for(String kredi : krediler) {
 			System.out.println(kredi);
 		}
 		
 		
-		// primitive variables - stack üzerinde tutuluyor.
+		// primitive variables - stack Ã¼zerinde tutuluyor.
 		int sayi1 = 10;
 		int sayi2 = 20;
 		sayi1 = sayi2;
 		sayi2 = 100;
 		System.out.println(sayi1);
 		
-		// Complex variables - refference type - hype üzerinde tutulur
+		// Complex variables - refference type - hype Ã¼zerinde tutulur
 		int[] sayilar1 = {1,2,3,4,5};
 		int[] sayilar2 = {10,20,30,40,50};
 		
@@ -40,11 +40,11 @@ public class Main {
 		
 		System.out.println(sayilar1[0]);
 		
-		// İstisnai durum. Burada referans tip olsa da basit değişkenler gibi davranır.
+		// Ä°stisnai durum. Burada referans tip olsa da basit deÄŸiÅŸik gibi davranÄ±r.
 		String s1 = new String("Ankara");
-		String s2 = new String("İstanbul");
+		String s2 = new String("Ä°stanbul");
 		s1 = s2;
-		s2 = "İzmir";
+		s2 = "Ä°zmir";
 		System.out.println(s1);
  
 	}
