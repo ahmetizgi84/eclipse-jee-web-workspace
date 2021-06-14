dd2e11c032cd722254bb9bde5ed7ba5b9ab13a7a
ahmetizgi <dev.ahmetizgi@gmail.com>
2021-06-14 20:49:09 +0300

    6. Postgresql - Spring boot

--- nLayeredDemo/src/nLayeredDemo/dataAccess/abstracts/ProductDao.java
+++ nLayeredDemo/src/nLayeredDemo/dataAccess/abstracts/ProductDao.java
@@ -14,7 +14,16 @@ public interface ProductDao {
 	Product get(int id);
 	
 	List<Product> getAll();
-
+	/**
+	 * int[] sayilar = {1,2,3,4,5};
+	 * 
+	 * yukarıda bir array tanımlandığı görülmektedir. Bu diziye bir değer 
+	 * eklenmek istenirse bu işlem gerçekleştirilemez. Nedeni ise 
+	 * değişken tanımlanırken dizinin uzunluğu belirlenmiştir yani sabittir.
+	 * Bu yüzden eklem veya çıkarma gibi işlemler yapılamaz. Bunun yerine
+	 * dinamik olarak ekleme ve çıkarma işlemi yapabilmemize imkan tanıyan
+	 * List<> yada ArrayList<> kullanılabilir.
+	 */
 	
 	
 
